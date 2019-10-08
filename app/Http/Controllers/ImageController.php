38e455954f3c320960d27dabd390cb8028ddb183
fabian <fabparada@gmail.com>
2019-10-07 23:39:57 -0300

    error no se permite agregar imagenes, lanza pantalla en blanco

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,22 +6,36 @@ use Illuminate\Http\Request;
 
 use App\Product;
 
+use App\ProductImage;
+
 class ImageController extends Controller
 {
     public function index($id)
-    {   
+    {
         $product = Product::find($id);
         $images = $product->images;
         return view('admin.products.images.index')->with(compact('product', 'images'));
     }
 
-    public function store()
+    public function store(Request $request, $id)
     {
-        
+        //guardar img en nuestro proyecto
+        $file = $request->file('photo');
+        $path = public_path() . '/images/products';
+        $fileName = uniqid() . $file->getClientOriginalName();
+        $file->move($path, $fileName);
+
+        //crear un registro en la bd en la tabla product_images
+        $productImage = new ProductImage();
+        $productImage->image = $fileName;
+        // $productImage->featured = false;
+        $productImage->product_id = $id;
+        $productImage->save(); //insert en la bd
+        return back();
     }
 
     public function destroy()
     {
-        
+
     }
 }
--- resources/views/admin/products/images/index.blade.php
+++ resources/views/admin/products/images/index.blade.php
@@ -10,17 +10,32 @@
 </div>
 <div class="main main-raised">
   <div class="container">
-
     <div class="section text-center">
-      <h2 class="title">imagenes de producto seleccionado</h2>
-      <a href="{{ '/admin/products/create' }}" class="mx-auto mb-3 btn btn-primary btn-round"type="button" name="button">Subir nueva imagen</a>
-    
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <img src="" alt="">
-            </div>
+      <h2 class="title">Imagenes del producto "{{ $product->nombre }}"</h2>
+      <!-- admin/products/4/images -->
+      <form method="post" action="" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input type="file" name="photo" value="" required>
+        <button type="submit" class=" mb-3 btn btn-primary btn-round">Subir nueva imagen</button>
+        <a href="{{ '/admin/products' }}" class=" mb-3 btn btn-primary btn-round">volver al listado de productos</a>
+      </form>
+      <hr>
+
+      <div class="row">
+        @foreach ($images as $image)
+
+        <div class="col-md-4 mb-5">
+          <div class="panel panel-default">
+              <div class="panel-body">
+                  <img src="{{ $image->image }}" alt="">
+                  <button type="submit" class="btn btn-danger btn-round">Eliminar imagen</button>
+              </div>
+          </div>
         </div>
-     
+
+      @endforeach
+      </div>
+
     </div>
   </div>
 </div>
